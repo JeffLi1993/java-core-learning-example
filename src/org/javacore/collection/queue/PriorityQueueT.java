2adc0d919519eca6f22c6217c18202c6c5ce56dd
BYSocket <949173650@qq.com>
2015-06-19 17:56:59 +0800

    Java集合Queue 2015年6月19日17:56:56
    Signed-off-by: BYSocket <949173650@qq.com>

---
+++ src/org/javacore/collection/queue/PriorityQueueT.java
@@ -0,0 +1,35 @@
+package org.javacore.collection.queue;
+
+import java.util.PriorityQueue;
+/*
+ * Copyright [2015] [Jeff Lee]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Jeff Lee
+ * @since 2015-6-17 23:32:30
+ * 	PriorityQueue的使用
+ */
+public class PriorityQueueT {
+	public static void main(String[] args) {
+		int[] ia = { 1, 10, 5, 3, 4, 7, 6, 9, 8 };
+		PriorityQueue<Integer> pq1 = new PriorityQueue<Integer>();
+ 
+		for (int x : ia) 
+			pq1.offer(x);
+		// 注意排序
+		System.out.println("pq1: " + pq1);
+	}
+}
