df2015d73714173c51f7fada58c1dc264291a68a
BYSocket <qiangqiangli1993@gmail.com>
2015-11-04 08:44:07 +0800

    后台线程遇到Finally

---
+++ src/org/javacore/thread/DaemonThreadFactory.java
@@ -0,0 +1,36 @@
+package org.javacore.thread;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Created by BYSocket on 2015/11/2.
+ *
+ * Copyright [2015] [Jeff Lee]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Jeff Lee
+ * @since 2015-11-3 18:17:49
+ * 	线程工厂类 - 将线程设置为后台线程
+ */
+public class DaemonThreadFactory implements ThreadFactory {
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.setDaemon(true);
+        return t;
+    }
+}
