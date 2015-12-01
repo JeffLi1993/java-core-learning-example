945f4b689dd4d8769a61a099bad55ebb6d45b2af
BYSocket <qiangqiangli1993@gmail.com>
2015-12-01 13:11:12 +0800

    新增：不使用接口的接口案例

---
+++ src/org/javacore/base/inter/ApplyClass.java
@@ -0,0 +1,59 @@
+package org.javacore.base.inter;
+
+import java.util.Arrays;
+
+/*
+ * Copyright [2015] [Jeff Lee]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Jeff Lee
+ * @since 2015-12-1 13:04:30
+ *  不使用接口的接口案例
+ */
+class Processor {
+    public String name(){
+        return getClass().getSimpleName();
+    }
+
+    Object process(Object input){return input;}
+}
+
+class Upcase extends Processor {
+    @Override
+    Object process(Object input) {
+        return ((String)input).toUpperCase();
+    }
+}
+
+class Splitcase extends Processor {
+    @Override
+    Object process(Object input) {
+        return Arrays.toString(((String) input).split(" "));
+    }
+}
+
+public class ApplyClass {
+    public static void process(Processor p , Object input){
+        System.out.println("调用对象名：" + p.name());
+        System.out.println(p.process(input));
+    }
+
+    public static String s = "BYSocket's Blog is www.bysocket.com";
+    public static void main(String[] args) {
+        process(new Upcase(),s);
+        process(new Splitcase(),s);
+    }
+}
