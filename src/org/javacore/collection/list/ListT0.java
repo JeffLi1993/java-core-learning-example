47567f9cc31a5b604adfacbe4d65555ba9928adf
BYSocket <qiangqiangli1993@gmail.com>
2016-01-06 22:49:06 +0800

    修正：ListIterator 的使用

--- src/org/javacore/collection/list/ListT0.java
+++ src/org/javacore/collection/list/ListT0.java
@@ -23,6 +23,8 @@ import java.util.ListIterator;
  * @since 2016-01-06 20:11:00
  *  打印list 全部元素，并找到其中元素aaa删除。要求：所有元素都遍历完
  *          ListIterator 的使用
+ *  list那个remove回抛异常是因为快速失败的机制，
+ *  他内置了一个modifyCount的变量记录list的长度变化，用Iter可以删除掉
  */
 public class ListT0 {
     public static void main(String[] args) {
