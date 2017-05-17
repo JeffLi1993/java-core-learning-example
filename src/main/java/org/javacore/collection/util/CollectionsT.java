fe69de1122064caf55ad46bdef411fda5134cda0
Roc <745656593@qq.com>
2017-05-17 19:03:51 +0800

    format

--- src/main/java/org/javacore/collection/util/CollectionsT.java
+++ src/main/java/org/javacore/collection/util/CollectionsT.java
@@ -9,9 +9,9 @@ import java.util.List;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,37 +24,37 @@ import java.util.List;
  * @since 2015-6-17 23:32:30
  * 	Collections的使用
  */
-public class CollectionsT{
-	public static void main(String[] args){
-		// 测试集合工具类Sort方法
-		testSort();
-		testAddAll();
-	}
-	
-	/**
-	 * 测试集合工具类Sort方法
-	 */
-private static void testSort(){
-	List<Integer> list = new ArrayList<Integer>();
-	list.add(1);
-	list.add(3);
-	list.add(2);
-	list.add(4);
-	// 调用集合工具类Sort方法
-	Collections.sort(list);
+public class CollectionsT {
+    public static void main(String[] args) {
+        // 测试集合工具类Sort方法
+        testSort();
+        testAddAll();
+    }
 
-	System.out.println("list sorted:");
-	System.out.print("\t" + list + "\n");
-}
+    /**
+     * 测试集合工具类Sort方法
+     */
+    private static void testSort() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(3);
+        list.add(2);
+        list.add(4);
+        // 调用集合工具类Sort方法
+        Collections.sort(list);
 
-private static void testAddAll() {
-	List<String> list = new ArrayList<>();
-	list.add("s2");
-	list.add("s4");
-	list.add("s1");
-	list.add("s3");
-	System.out.println(list);
-	Collections.addAll(list, "s5","s7",null,"s9");
-	System.out.println(list);
-}
+        System.out.println("list sorted:");
+        System.out.print("\t" + list + "\n");
+    }
+
+    private static void testAddAll() {
+        List<String> list = new ArrayList<>();
+        list.add("s2");
+        list.add("s4");
+        list.add("s1");
+        list.add("s3");
+        System.out.println(list);
+        Collections.addAll(list, "s5", "s7", null, "s9");
+        System.out.println(list);
+    }
 }
